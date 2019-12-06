ec93a9dfd804868c68fabc3514ba38df1a1c0a1a
beata <beata@emc2wave.com>
2019-12-06 12:37:58 -0800

    add setButtonPosition

--- script.js
+++ script.js
@@ -14,7 +14,18 @@ document.addEventListener('mouseover', (e) => {
   const verticalDistanceForm = distanceFromCenter(buttonBox.y, y, buttonBox.height);
   const horizontalOffset = buttonBox.width / 2 + OFFSET;
   const verticalOffset = buttonBox.height / 2 + OFFSET;
+  if(Math.abs(horizontalDistanceForm) <= horizontalOffset && Math.abs(verticalDistanceForm) <= verticalOffset) {
+    setButtonPosition(
+      buttonBox.x + horizontalOffset / horizontalDistanceForm * 10,
+      buttonBox.y + verticalOffset / verticalDistanceForm * 10,
+    )
+  }
 })
+
+function setButtonPosition(left,top) {
+  evilButton.style.left = `${left}px`
+  evilButton.style.top = `${top}px`
+}
 
 function distanceFromCenter(boxPosition, mousePosition, boxSize) {
   return boxPosition - mousePosition + boxSize / 2

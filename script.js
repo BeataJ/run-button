92f44086d3fe38c02d8599bd6572a764c4c20ea7
beata <beata@emc2wave.com>
2019-12-06 11:16:17 -0800

    add event Listener mouseover on document

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ evilButton.addEventListener('click', () => {
   alert('Nice Try')
   window.close()
 })
+
+document.addEventListener('mouseover', (e) => {
+  const x = e.pageX
+  const y = e.pageY
+  console.log(x, y)
+})
